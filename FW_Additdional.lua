de7859a14e765b2f5ed7a05f2ea775ea4bccf59e
Grisu118 <grisu118.ls2011@gmail.com>
2013-12-10 20:07:16 +0100

    Link hierher nachgetragen

--- FW_Additdional.lua
+++ FW_Additdional.lua
@@ -3,6 +3,7 @@
 -- @author  Grisu118
 -- @date  11/04/2013
 -- @version 0.9
+-- @Descripion: Readme you can find there: https://github.com/Grisu118/Scripts
 -- Copyright (C) Grisu118, All Rights Reserved.
 -- free for noncommerical-usage
 
