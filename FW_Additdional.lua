21dfa7af9c7c212b48af74a348d3b759073d7488
Grisu118 <grisu118.ls2011@gmail.com>
2013-12-27 22:37:07 +0100

    First try to add FuelIndicator
    Not running at the Moment

--- FW_Additdional.lua
+++ FW_Additdional.lua
@@ -1,22 +1,54 @@
--- 
+-- Special Things for Fire Deparment Vehicles
 --
 -- @author  Grisu118
--- @date  11/04/2013
--- @version 0.9
+-- @date  27/12/2013
+-- @version 0.91
 -- @Descripion: Readme you can find there: https://github.com/Grisu118/Scripts
 -- @web: http://grisu118.ch or http://vertexdezign.de
 -- Copyright (C) Grisu118, All Rights Reserved.
 -- free for noncommerical-usage
 
+--[[
+<fuelIndicator>
+		<indicator index="0>0" />
+		<indicator index="0>1" />
+		<indicator index="0>2" />
+</fuelIndicator>
+
+]]
+
 
 FW_Additdional = {};
 
 function FW_Additdional.prerequisitesPresent(specializations)
-	return SpecializationUtil.hasSpecialization(Motorized, specializations);
+	return SpecializationUtil.hasSpecialization(Motorized, specializations) and SpecializationUtil.hasSpecialization(Fillable, specializations);
 end;
 function FW_Additdional:load(xmlFile)	
 	self.Mannschaft = Utils.indexToObject(self.components, getXMLString(xmlFile, "vehicle.Mannschaft#index"));
 	self.drl = Utils.indexToObject(self.components, getXMLString(xmlFile, "vehicle.drl#index"));
+	
+	--Fuel Indicator
+	self.count = 0;
+	self.percent = 0;
+	local i = 0;
+	self.fuelIndicators = {};
+	while true do
+		local path = string.format("vehicle.fuelIndicator.indicator(%d)", i);
+		local component = Utils.indexToObject(self.components, getXMLString(xmlFile, path .. "#index"));	
+		if component == nil then
+			break;
+		end;
+		table.insert(self.fuelIndicators, component);
+		i = i + 1;
+	end;	
+
+		self.count = table.getn(self.fuelIndicators);
+		self.percent = 100 / self.count;
+	
+		for j = 0, self.count, 1	do
+			setVisibility(self.fuelIndicators[j], false);
+		end;
+
 	
 	setVisibility(self.Mannschaft, false);
 	setVisibility(self.drl, false);
@@ -28,6 +60,16 @@ function FW_Additdional:readStream(streamId, connection)
 end;
 function FW_Additdional:writeStream(streamId, connection)
 end;
+
+function FW_Additdional:loadFromAttributesAndNodes(xmlFile, key, resetVehicles)
+	local fillLevel = getXMLFloat(xmlFile, key .. "#fillLevel");
+	if fillLevel ~= nil then
+-- TODO Set Fuel indicator from xml while Loading
+	end;
+	
+	return BaseMission.VEHICLE_LOAD_OK;
+end;
+
 function FW_Additdional:mouseEvent(posX, posY, isDown, isUp, button)
 end;
 function FW_Additdional:keyEvent(unicode, sym, modifier, isDown)
@@ -40,6 +82,12 @@ function FW_Additdional:updateTick(dt)
 			setVisibility(self.drl, true);
 		else
 			setVisibility(self.drl, false);
+		end;
+		
+		if self.count > 0 then
+-- TODO Change Fuel Indicator
+-- Attention Fuel Indicator only look to own filllevel not to fillevel of a trailer!
+--			if self.fillLevel == 0 then
 		end;
 	end;
 end;
