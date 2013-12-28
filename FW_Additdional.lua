e7fd3da9f48d2f9c6d16f066778092de0d7d5689
Grisu118 <grisu118.ls2011@gmail.com>
2013-12-28 13:28:00 +0100

    fuelIndicator now works, also drl

--- FW_Additdional.lua
+++ FW_Additdional.lua
@@ -1,19 +1,23 @@
 -- Special Things for Fire Deparment Vehicles
 --
 -- @author  Grisu118
--- @date  27/12/2013
--- @version 0.91
+-- @date  28/12/2013
+-- @version 1.0
 -- @Descripion: Readme you can find there: https://github.com/Grisu118/Scripts
 -- @web: http://grisu118.ch or http://vertexdezign.de
 -- Copyright (C) Grisu118, All Rights Reserved.
 -- free for noncommerical-usage
 
 --[[
-<fuelIndicator>
-		<indicator index="0>0" />
-		<indicator index="0>1" />
+<fuelIndicator firstIndicator="0>4"> <!-- First lamp, is on when tank is not empty -->
+		<indicator index="0>0" /> <!-- second -- The other lamps, you can set so much as you want, here are three lamps so first is on when more than 33% is in the tank ... -->
+		<indicator index="0>1" /> <!-- third -->
 		<indicator index="0>2" />
 </fuelIndicator>
+
+<drl index="0>15|12" /> <!-- Day Drive light, is on when motor is started-->
+
+<crew index="0>16|1" /> <!-- Crew, is visible when you are in the car -->
 
 ]]
 
@@ -21,15 +25,28 @@
 FW_Additdional = {};
 
 function FW_Additdional.prerequisitesPresent(specializations)
+	if not SpecializationUtil.hasSpecialization(Motorized, specializations) then
+		print("Warning: Specialization FW_Additdional needs the specialization Motorized.");
+	end;
+	if not SpecializationUtil.hasSpecialization(Fillable, specializations) then
+		print("Warning: Specialization FW_Additdional needs the specialization Fillable.");
+	end;
+
 	return SpecializationUtil.hasSpecialization(Motorized, specializations) and SpecializationUtil.hasSpecialization(Fillable, specializations);
 end;
-function FW_Additdional:load(xmlFile)	
-	self.Mannschaft = Utils.indexToObject(self.components, getXMLString(xmlFile, "vehicle.Mannschaft#index"));
+function FW_Additdional:load(xmlFile)
+	--crew
+	self.crew = Utils.indexToObject(self.components, getXMLString(xmlFile, "vehicle.crew#index"));
+	setVisibility(self.crew, false);
+	
+	--drl (day Drive Light)
 	self.drl = Utils.indexToObject(self.components, getXMLString(xmlFile, "vehicle.drl#index"));
+	setVisibility(self.drl, false);
 	
 	--Fuel Indicator
 	self.count = 0;
 	self.percent = 0;
+	self.firstIndicator = Utils.indexToObject(self.components, getXMLString(xmlFile, "vehicle.fuelIndicator#firstIndicator"));
 	local i = 0;
 	self.fuelIndicators = {};
 	while true do
@@ -42,16 +59,14 @@ function FW_Additdional:load(xmlFile)
 		i = i + 1;
 	end;	
 
-		self.count = table.getn(self.fuelIndicators);
-		self.percent = 100 / self.count;
+	self.count = table.getn(self.fuelIndicators);
+	self.percent = 100 / self.count;
 	
-		for j = 0, self.count, 1	do
-			setVisibility(self.fuelIndicators[j], false);
-		end;
-
+	setVisibility(self.firstIndicator, false);
+	for j = 0, self.count, 1	do
+		setVisibility(self.fuelIndicators[j], false);
+	end;
 	
-	setVisibility(self.Mannschaft, false);
-	setVisibility(self.drl, false);
 end;
 
 function FW_Additdional:delete()
@@ -77,7 +92,7 @@ end;
 function FW_Additdional:update(dt)
 end;
 function FW_Additdional:updateTick(dt)
-	if self:getIsActive() then
+--	if self:getIsActive() then
 		if self.isMotorStarted then
 			setVisibility(self.drl, true);
 		else
@@ -87,9 +102,29 @@ function FW_Additdional:updateTick(dt)
 		if self.count > 0 then
 -- TODO Change Fuel Indicator
 -- Attention Fuel Indicator only look to own filllevel not to fillevel of a trailer!
---			if self.fillLevel == 0 then
+			--self.fillLevel = Fillable.getFillLevel;
+			--print (self.fillLevel);
+			
+			if self.fillLevel <= 0 then
+				setVisibility(self.firstIndicator, false);
+				for j = 0, self.count, 1	do
+					setVisibility(self.fuelIndicators[j], false);
+				end;
+			else 
+				local fillpercent = (self.fillLevel) / (self.capacity) * 100;
+				--print (fillpercent);
+				setVisibility(self.firstIndicator, true);
+				local lamps = math.floor(fillpercent / (self.percent));
+				--print (lamps);
+				for j = 0, lamps, 1 do
+					setVisibility(self.fuelIndicators[j], true);
+				end;
+				for j = lamps + 1, self.count, 1 do
+					setVisibility(self.fuelIndicators[j], false);
+				end; 
+			end;
 		end;
-	end;
+--	end;
 end;
 
 function FW_Additdional:draw()
@@ -99,7 +134,7 @@ end;
 function FW_Additdional:detachImplement(implementIndex)
 end;
 function FW_Additdional:onEnter()
-			setVisibility(self.Mannschaft, true);
+			setVisibility(self.crew, true);
 		if self.isMotorStarted then	
 			setVisibility(self.drl, true);
 		else
@@ -107,7 +142,7 @@ function FW_Additdional:onEnter()
 		end
 end;
 function FW_Additdional:onLeave()
-			setVisibility(self.Mannschaft, false);
+			setVisibility(self.crew, false);
 	    if self.isMotorStarted then
 			setVisibility(self.drl, true);
 		else			
